de3c512d505cc7e4f9f5aed231c92e0ad8211310
Alex Pereira <alexapereira14@gmail.com>
2022-06-15 08:10:34 -0400

    feat: add sass loader

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 
-import './Counter.css'
+import './Counter.scss'
 
 export const Counter = () => {
   const [count, setCount] = useState<number>(0)
 
   return (
-    <div>
+    <div className='counter-container'>
       <h3 className='the-heading'>Update the count and edit src/App.tsx, state is preserved!!!</h3>
+      <h3 className='the-heading green'>Update the count and edit src/App.tsx, state is preserved!!!</h3>
       <button
         data-testid='counter-button'
         onClick={() => setCount((c) => c + 1)}
